25b2ffb82c1d648ca10285da8e0d02f193bba968
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2026-02-13 16:19:10 +0100

    Bump dependencies for Laravel 10/11/12 support, drop Laravel 7/8/9 and PHP 7.4

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,10 +3,11 @@
 namespace SteadfastCollective\LaravelDailyco\Tests;
 
 use Orchestra\Testbench\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 
 class ExampleTest extends TestCase
 {
-    /** @test */
+    #[Test]
     public function true_is_true()
     {
         $this->assertTrue(true);
